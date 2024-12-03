48534bf4db4a253b8cdfff61b364ec257c4aa8a0
Lautaro Poletto <lautaro.poletto@nubiral.com>
2024-12-03 17:00:28 -0300

    Add: pipelines de adf y azuresynapse

---
+++ pipelines/azuresynapse/sp_CreateSQLServerlessView_gold.sql
@@ -0,0 +1,19 @@
+USE gold_db
+GO
+
+CREATE OR ALTER PROC CreateSQLServerlessView_gold @ViewName NVARCHAR(100)
+AS
+BEGIN
+    DECLARE @statement VARCHAR(MAX)
+    SET @statement = N'CREATE OR ALTER VIEW ' + 'VW_' + @ViewName + ' AS
+        SELECT 
+            *
+        FROM
+            OPENROWSET(
+                BULK ''https://sgetlproject.dfs.core.windows.net/gold/SalesLT/' + @ViewName + '/'',
+                FORMAT = ''DELTA''
+            ) AS [result]'
+
+    EXEC (@statement)
+END
+GO
